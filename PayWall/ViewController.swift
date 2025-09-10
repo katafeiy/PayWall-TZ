32252bd0f417180ed54604d9f7db28383eb56dbd
Filippov Konstantin <fkv.katafey@yandex.ru>
2025-09-10 11:57:49 +0400

    Initial Commit

---
+++ PayWall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayWall
+//
+//  Created by Константин Филиппов on 10.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
